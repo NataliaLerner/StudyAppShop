670664ac54f852379ab2c2c78d9461edf0d820be
Иван Коваленко <ivankovalenko@MacBook-Ivan.local>
2019-03-21 21:20:38 +0300

    [add] Добавил шаблон страниц "Главная" и "Товары"

--- start.py
+++ start.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 
 app = Flask(__name__)
+app.secret_key = '5e8d527e-7dc4-4be5-8364-44ae3dcb43d0'
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+	session['shopping'] = {'1':[],'2':[]}
+	return render_template('index.html')
 
-@app.route('/main')
-def main():
-	return 'main'
+@app.route('/shop')
+def shop():
+	count_shop = len(session['shopping'])
+	return render_template('shop.html', count_shop = count_shop)
